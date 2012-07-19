934f5c5ea24044677e8f68553d139b63de8aa95e
Alexey Rebrov <abnormy@mail.ru>
2012-07-19 10:48:06 +0300

    Working copy of js test runner

--- Tests/js/runner.js
+++ Tests/js/runner.js
@@ -1,14 +1,15 @@
 console.log('Loading a web page');
 var page = new WebPage();
-var url = phantom.args[0];
-page.onConsoleMessage = function(msg) {console.log(msg);};
+var url = 'file:///' + phantom.args[0] + '/Tests/js/runner.htm';
+page.onConsoleMessage = function(msg) {
+	if (msg && msg.indexOf("##jasmine.reportRunnerResults") !== -1) {
+        phantom.exit();
+    }
+	console.log(msg);
+};
 page.open(url, function (status) {
 	if(status !== 'success') {
 		console.log('Unable to load suite');
-	}
-	else {
-		window.setTimeout(function() {
-			phantom.exit();
-		}, 200);
+		phantom.exit();
 	}
 });
